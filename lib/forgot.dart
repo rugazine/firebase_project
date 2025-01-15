91f609d85035d6f860b4feb67ec6db7f928bf2ee
Jojo <jovancojojo268@gmail.com>
2025-01-15 09:08:41 +0700

    feat/sign-in-google-CRI

--- lib/forgot.dart
+++ lib/forgot.dart
@@ -9,47 +9,29 @@ class Forgot extends StatefulWidget {
 }
 
 class _ForgotState extends State<Forgot> {
-  TextEditingController email = TextEditingController();
-  String message = ''; // Menyimpan pesan untuk ditampilkan
 
-  // Fungsi untuk mereset password
-  reset() async {
-    try {
-      await FirebaseAuth.instance.sendPasswordResetEmail(email: email.text);
-      setState(() {
-        message = 'Link reset password telah dikirim ke email Anda!';
-      });
-    } catch (e) {
-      setState(() {
-        message = 'Terjadi kesalahan: ${e.toString()}';
-      });
-    }
-  }
+TextEditingController email=TextEditingController();
+
+reset()async{
+  await FirebaseAuth.instance.sendPasswordResetEmail(email: email.text);
+}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Forgot password")),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
+       appBar: AppBar(title: Text("Forgot password"),),
+       body: Padding(
+         padding: const EdgeInsets.all(20.0),
+         child: Column(
           children: [
             TextField(
               controller: email,
               decoration: InputDecoration(hintText: 'Enter email'),
             ),
-            ElevatedButton(
-              onPressed: reset,
-              child: Text("Send link"),
-            ),
-            SizedBox(height: 20),
-            Text(
-              message,
-              style: TextStyle(color: message.contains('error') ? Colors.red : Colors.green),
-            ),
+            ElevatedButton(onPressed: (()=>reset()), child: Text("Sign up"))
           ],
-        ),
-      ),
+         ),
+       ),
     );
   }
 }
--- lib/login.dart
+++ lib/login.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:project_firebase/forgot.dart';
 import 'package:project_firebase/signup.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -12,21 +13,38 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
 
-TextEditingController email=TextEditingController();
-TextEditingController password=TextEditingController();
+  signIn() async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email.text, password: password.text);
+  }
 
-signIn()async{
-  await FirebaseAuth.instance.signInWithEmailAndPassword(email: email.text, password: password.text);
-}
+  Future<void> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      await FirebaseAuth.instance.signInWithCredential(credential);
+    } catch (e) {
+      print('Error signing in with Google: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(title: Text("Login"),),
-       body: Padding(
-         padding: const EdgeInsets.all(20.0),
-         child: Column(
+      appBar: AppBar(title: Text("Login")),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
           children: [
             TextField(
               controller: email,
@@ -36,14 +54,22 @@ signIn()async{
               controller: password,
               decoration: InputDecoration(hintText: 'Enter password'),
             ),
-            ElevatedButton(onPressed: (()=>signIn()), child: Text("Login")),
-            SizedBox(height: 30,),
-            ElevatedButton(onPressed: (()=>Get.to(Signup())), child: Text("Register now")),
-            SizedBox(height: 30,),
-            ElevatedButton(onPressed: (()=>Get.to(Forgot())), child: Text("Forgot password ?"))
+            ElevatedButton(onPressed: (() => signIn()), child: Text("Login")),
+            SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: (() => signInWithGoogle()),
+                child: Text("Login with Google")),
+            SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: (() => Get.to(Signup())),
+                child: Text("Register now")),
+            SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: (() => Get.to(Forgot())),
+                child: Text("Forgot password ?"))
           ],
-         ),
-       ),
+        ),
+      ),
     );
   }
 }
